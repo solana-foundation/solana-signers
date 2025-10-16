af2c08476c03c5a75b9d32e29141c7fa8a0fc038
amilz <85324096+amilz@users.noreply.github.com>
2025-10-16 13:45:12 -0700

    feat: add vault TS
    +chore: test vault

---
+++ typescript/packages/vault/src/__tests__/setup.ts
@@ -0,0 +1,31 @@
+import { SignerTestConfig, TestScenario } from '@solana-signers/test-utils';
+import { VaultSigner } from '../vault-signer';
+
+const SIGNER_TYPE = 'vault';
+const REQUIRED_ENV_VARS = [
+    'VAULT_ADDR',
+    'VAULT_TOKEN',
+    'VAULT_KEY_NAME',
+    'VAULT_SIGNER_PUBKEY',
+];
+
+async function createVaultSigner(): Promise<VaultSigner> {
+    return new VaultSigner({
+        vaultAddr: process.env.VAULT_ADDR!,
+        vaultToken: process.env.VAULT_TOKEN!,
+        keyName: process.env.VAULT_KEY_NAME!,
+        publicKey: process.env.VAULT_SIGNER_PUBKEY!,
+    });
+}
+const CONFIG: SignerTestConfig<VaultSigner> = {
+    signerType: SIGNER_TYPE,
+    requiredEnvVars: REQUIRED_ENV_VARS,
+    createSigner: createVaultSigner,
+};
+
+export async function getConfig(scenarios: TestScenario[]): Promise<SignerTestConfig<VaultSigner>> {
+    return {
+        ...CONFIG,
+        testScenarios: scenarios,
+    };
+}
